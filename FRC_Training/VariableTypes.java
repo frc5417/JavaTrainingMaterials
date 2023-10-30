cc51e540f212bf80ddecfc3a22153b4d4779aaaa
Jkvelagapudi <111027250+Jkvelagapudi@users.noreply.github.com>
2023-10-30 17:51:10 -0500

    Add files via upload

---
+++ FRC_Training/VariableTypes.java
@@ -0,0 +1,18 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+        String variable1 = "Robotics";
+        int variable2 = 1234;
+        double variable3 = 6.75;
+        char variable4 = 'A';
+        boolean variable5 = true;
+        float variable6 = 5.5678f;
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+    }
+}
+
