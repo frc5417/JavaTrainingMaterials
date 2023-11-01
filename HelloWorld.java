a88ba14e8476fad9c13a8b78151ab2781fefc12f
Jeevan Adhya Vinoth Babu <jeevanadhya.vinothbabu@gmail.com>
2023-10-31 19:20:43 -0500

    Add user input to Hello World.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Enter something in the next line!");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("You entered: " + userInput.next());
     }
 }
 
